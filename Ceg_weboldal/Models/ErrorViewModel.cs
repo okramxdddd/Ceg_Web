1a9450fae16e235f9bf9de9049b1c33967e03eb8
Bottyán Vak <ecdl01@vbjnet.hu>
2025-12-15 13:54:49 +0100

    asdasd

---
+++ Ceg_weboldal/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Ceg_weboldal.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
